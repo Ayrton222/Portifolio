8852a33a8ae1c95113612fb513a12826d2f5942e
Ayrton Senna <73143891+Ayrton222@users.noreply.github.com>
2023-01-01 20:32:23 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ NavBarResponsiva();
 
 function NavBarResponsiva() {
     const BotaoMenu = document.getElementsByClassName('botaomenu')[0];
-    const navbarLinks = document.getElementsByClassName('navbar-links')[0];
+    const navbarLinks = document.getElementsByClassName('barra-nevacao')[0];
 
     BotaoMenu.addEventListener('click', () => {
         navbarLinks.classList.toggle('active');
